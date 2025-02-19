a756b865ca723083017dc5d9f46197ca339af24f
lea210605 <sch210605@spengergasse.at>
2025-02-19 15:08:38 +0100

    Initial Push - Template

---
+++ vue-start-template/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container mt-3 p-4 bg-danger text-white rounded text-center">
+    <h1>Vue.js Demo App <i class="bi bi-hand-thumbs-up-fill"></i></h1>
+  </div>
+</template>
+
+<script>
+</script>
